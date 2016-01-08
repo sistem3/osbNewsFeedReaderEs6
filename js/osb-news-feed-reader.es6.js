2875f13d457f1ba4a536c45c5e08f0def6b0328c
Ben Burton <sistematic3@yahoo.co.uk>
2016-01-08 16:47:36 +0000

    Adding basic styles

--- js/osb-news-feed-reader.es6.js
+++ js/osb-news-feed-reader.es6.js
@@ -1,10 +1,20 @@
 "use strict";
 
 (function() {
-    let template = ``;
+    let template = `
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Roboto:400,300');
+            @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
+            @import '/css/osb-news-feed-reader.css';
+        </style>
+        <main>
+            <h2>News <i class="fa fa-newspaper-o"></i></h2>
+            <ul class="feed-list"></ul>
+        </main>`;
     class osbNewsFeedReader extends HTMLElement {
         createdCallback() {
             this.createShadowRoot().innerHTML = template;
+            this.getNewsFeed();
         };
 
         attachedCallback() {
@@ -12,6 +22,14 @@
 
         attributeChangedCallback(attrName, oldVal, newVal) {
 
+        };
+
+        getNewsFeed(feed) {
+            console.log(feed);
+            fetch('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=')
+                .then(function() {
+
+                });
         };
 
     }
